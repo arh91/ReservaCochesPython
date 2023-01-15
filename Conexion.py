e86e3f03120831387fce8f08beea254be03d4fe7
arh91 <alvaro_rh_991@hotmail.com>
2023-01-15 11:24:38 +0100

    En Conexion.py, comprobamos que se establezca correctamente la conexión entre Python y MySQL

--- Conexion.py
+++ Conexion.py
@@ -1,12 +1,28 @@
+"""Comprobamos que se establezca correctamente la conexión entre Python y MySQL"""
+
 import mysql.connector
 
-conexion = mysql.connector.connect(
-    user='alvaro',
-    password='castelao',
-    host='localhost',
-    database='UD02BDReservaCoches',
-    port='3306')
+try:
+    conexion = mysql.connector.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='castelao',
+        db='UD02BDReservaCoches')
 
-print(conexion)
+    if conexion.is_connected():
+        print("Conexión exitosa")
+        info_server = conexion.get_server_info()
+        print(info_server)
+        cursor = conexion.cursor()
+        cursor.execute("SELECT DATABASE()")
+        row = cursor.fetchone()
+        print("Conectado a la base de datos: {}".format(row))
+except Exception as ex:
+    print(ex)
+finally:
+    if conexion.is_connected():
+        conexion.close()
+        print("Conexión finalizada.")
 
 
